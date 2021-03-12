e56b57e9173b47b72572c6e27dcd5c282319c1f0
Jon Shapiro <jon@datagarden.org>
2021-03-12 13:13:33 -0800

    changed tailwind.config.js to es6 module

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
-module.exports = {
-  purge: [],
-  theme: {
-    fontFamily: {
-      sans: "Roboto, Arial, sans-serif",
-      serif: "Merriweather, Georgia, serif",
-    },
-    extend: {},
+export const purge = [];
+export const theme = {
+  fontFamily: {
+    sans: "Roboto, Arial, sans-serif",
+    serif: "Merriweather, Georgia, serif",
   },
-  variants: {},
-  plugins: [],
+  extend: {},
 };
+export const variants = {};
+export const plugins = [];
